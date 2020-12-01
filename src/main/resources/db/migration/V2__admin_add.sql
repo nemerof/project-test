19b0b6d7822f1e4ea87e12874fc0802aa282b0da
nemerof <nemerof716@gmail.com>
2020-12-01 16:21:52 +0200

    changing db migration for correct admin photo

--- src/main/resources/db/migration/V2__admin_add.sql
+++ src/main/resources/db/migration/V2__admin_add.sql
@@ -1,5 +1,5 @@
-insert into usr (id, username, password, active, email)
-values (1, 'admin', 'admin123', true, 'admin123@gmail.com');
+insert into usr (id, username, password, active, email, profile_pic)
+values (1, 'admin', 'admin123', true, 'admin123@gmail.com', 'default-profile-icon.png');
 
 insert into user_role (user_id, roles)
 values (1, 'USER'), (1, 'ADMIN');
