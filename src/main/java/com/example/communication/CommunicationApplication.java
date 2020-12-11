b2e7bd260b5d0bea16ec2b5e9894e337a2a89c5d
nemerof <nemerof716@gmail.com>
2020-12-11 15:58:52 +0200

    small fix in default profile pic upload

--- src/main/java/com/example/communication/CommunicationApplication.java
+++ src/main/java/com/example/communication/CommunicationApplication.java
@@ -6,22 +6,29 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommunicationApplication {
+  private static final String classPathFirst = System.getProperty("java.class.path");
+  private static final String realClassPath = classPathFirst.substring(0, classPathFirst.indexOf("target"))
+                        + "src\\main\\resources\\static\\images\\default-profile-icon.png";
+  private static final String uploadPath = "/D:/communication";
 
   public static void main(String[] args) throws IOException {
-    try (
-        InputStream from = new FileInputStream(
-            new File("/home/friday58/IdeaProjects/communication/src/main/resources/static/images/default-profile-icon.png"));
-        OutputStream to = new FileOutputStream(new File("/home/friday58/Documents/upload_dir/default-profile-icon.png")))
-    {
-      byte[] buffer = new byte[1024];
-      int length;
-      while ((length = from.read(buffer)) > 0) {
-        to.write(buffer, 0, length);
+    File defaultPhotoFile = new File(realClassPath);
+    if (!defaultPhotoFile.exists()) {
+      try (
+              InputStream from = new FileInputStream(defaultPhotoFile);
+              OutputStream to = new FileOutputStream(new File(uploadPath + "/default-profile-icon.png"))) {
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = from.read(buffer)) > 0) {
+          to.write(buffer, 0, length);
+        }
       }
     }
     SpringApplication.run(CommunicationApplication.class, args);
