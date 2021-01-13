c845f79705dc57e62a31971617d8326f787dd856
Artyom <divinefridaay@mail.ru>
2021-01-13 22:51:03 +0300

    Added service test classes

---
+++ src/test/java/com/example/communication/service/UserServiceTest.java
@@ -0,0 +1,26 @@
+package com.example.communication.service;
+
+import org.junit.jupiter.api.Test;
+
+class UserServiceTest {
+
+  @Test
+  void getAllUsers() {
+  }
+
+  @Test
+  void getAllUsersTest() {
+  }
+
+  @Test
+  void loadUserByUsername() {
+  }
+
+  @Test
+  void editUserInUserList() {
+  }
+
+  @Test
+  void deleteUserInUserList() {
+  }
+}
